f24d222c925e2721c5b80cbc1f6de9ab8ce3b802
Demetria-PC\Demetria <dongmeima01@gmail.com>
2017-09-07 10:18:42 +0800

    后序遍历二叉树

--- algorithm/src/com/binarytree/ZXBLBianryTree.java
+++ algorithm/src/com/binarytree/ZXBLBianryTree.java
@@ -11,17 +11,38 @@ import java.util.Stack;
 public class ZXBLBianryTree {
 	public static void main(String[] args) {
 		BinaryTree root=BinaryTree.getInstance();
-		
+		zxbl(root);
+		System.out.println();
+		dg_zxbl(root);
 	}
 	
+	//·ÇµÝ¹é
 	public static void zxbl(BinaryTree root){
 		if(root==null)
 			return;
 		BinaryTree p=root;
 		Stack<BinaryTree> s=new Stack<BinaryTree>();
-		s.push(p);
 		while(p!=null || !s.isEmpty()){
-			
+			if(p!=null){
+				s.push(p);
+				p=p.left;
+			}
+			else{
+				p=s.pop();
+				System.out.print(p.val);
+				p=p.right;
+			}
 		}
+	}
+	
+	//µÝ¹é
+	public static void dg_zxbl(BinaryTree root){
+		if(root==null)
+			return;
+		if(root.left!=null)
+			dg_zxbl(root.left);
+		System.out.print(root.val);
+		if(root.right!=null)
+			dg_zxbl(root.right);
 	}
 }
