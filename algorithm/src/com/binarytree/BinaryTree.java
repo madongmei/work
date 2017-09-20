a2a7b1a0f6e6b37376720747a5345a8a8a721286
Demetria-PC\Demetria <dongmeima01@gmail.com>
2017-09-20 09:55:03 +0800

    Revert "no message"
    This reverts commit abb3c70c14b1c9aa4249f7a5a33f65bf8a1cfaef.

--- algorithm/src/com/binarytree/BinaryTree.java
+++ algorithm/src/com/binarytree/BinaryTree.java
@@ -11,19 +11,19 @@ public class BinaryTree {
 	
 	public static BinaryTree getInstance(){
 		BinaryTree root=new BinaryTree(1);
-		BinaryTree r1=new BinaryTree(1);
-		BinaryTree r2=new BinaryTree(2);
-		BinaryTree r3=new BinaryTree(3);
-		BinaryTree r4=new BinaryTree(4);
-		BinaryTree r5=new BinaryTree(5);
-		BinaryTree r6=new BinaryTree(6);
+		BinaryTree r1=new BinaryTree(2);
+		BinaryTree r2=new BinaryTree(3);
+		BinaryTree r3=new BinaryTree(4);
+		BinaryTree r4=new BinaryTree(5);
+		BinaryTree r5=new BinaryTree(6);
+		BinaryTree r6=new BinaryTree(7);
 		
 		root.left=r1;
 		root.right=r2;
 		r1.left=r3;
-		r2.right=r4;
-		r3.right=r5;
-		r5.left=r6;
+		r1.right=r4;
+		r2.left=r5;
+		r2.right=r6;
 		
 		return root;
 	}
