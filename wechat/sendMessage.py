7da57350a22f1fd110f7abc60d336419c03ca014
Demetria <Demetria_M@bupt.edu.cn>
2017-10-24 20:34:32 +0800

    发送消息

--- wechat/sendMessage.py
+++ wechat/sendMessage.py
@@ -7,24 +7,33 @@
 @file: sendMessage.py
 @time: 2017/10/23 11:43
 """
-
+import sys
 import logging
+from logging.handlers import RotatingFileHandler
 import json
 import requests
 import MySQLdb
 import time
+import datetime
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 APPID = 'wx18862d70a1b81b27'
 SECRET = '1f3e6119fd1f5f8aed5fbb75beb675c1'
 OPENID = 'oi9jY0gGdx0_JauMDnvTRNri3vWU'
 
 
-logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='sendMessage.log',
-                filemode='w')
-dt = time.strftime("%Y%m%d%H", time.localtime(time.time()))
+rt_file_handler=RotatingFileHandler("sendMessage.log", maxBytes=10*1024*1024, backupCount=5)
+rt_file_handler.setLevel(logging.DEBUG)
+formatter=logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+rt_file_handler.setFormatter(formatter)
+logger=logging.getLogger()
+logger.addHandler(rt_file_handler)
+logger.setLevel(logging.DEBUG)
+
+dt = ((datetime.datetime.now()-datetime.timedelta(hours=1)).strftime("%Y%m%d%H"))
+#dt = time.strftime("%Y%m%d%H", time.localtime(time.time()))
 
 def connect_db():
     conn = MySQLdb.connect(
@@ -46,12 +55,16 @@ def getToken():
     return TOKEN
 
 def sendMsg():
+    msg = getContent()
+    if msg is None:
+        logger.info("no danger info")
+        return
     payload = {
         "touser":OPENID,
         "msgtype":"text",
         "text":
         {
-            "content":getContent()
+            "content":msg
         }
     }
     TOKEN = getToken()
@@ -64,27 +77,33 @@ def getDangerOP():
     try:
         conn=connect_db()
         cursor=conn.cursor()
-        sql=("select count(level), info from alarm_log where ds=%s group by level" %(2017090811))
-        logging.info(sql)
+        sql=("select count(*),info from alarm_log where level>=1 and ds=%s group by info" %(str(dt)))
+        logger.info(sql)
         cursor.execute(sql)
         results = cursor.fetchall()
-        for row in results:
-            level = str(row[0])
-            info = str(row[1])
-            msg = msg + '操作级别:' + level + ', ' + '警告信息:' + info +'\n'
+        if len(results)>0:
+            for row in results:
+                level = str(row[0])
+                info = str(row[1])
+                msg = u'%s 操作级别: %s, 告警信息: %s\n\n' %(msg, level, info)
+        else:
+            return None
         conn.commit()
     except Exception, e:
-        logging.error("Mysql Error %s",e.args[0])
+        logger.error("Mysql Error %s",e.args[0])
         conn.rollback()
     
     cursor.close()
     conn.close()
-    return msg
+    return msg.encode("utf-8")
 
 def getContent():
-    msg = '时间：' + str(dt) + "\n" + getDangerOP()
-    logging.info(msg)
-    return msg
+    info = getDangerOP()
+    if info is None:
+        return None
+    msg = u'时间: %s\n %s' %(str(dt), info)
+    logger.info(msg)
+    return msg.encode('utf-8')
 
 
 def main():
