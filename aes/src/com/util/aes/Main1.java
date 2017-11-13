8acae3635573a103ac37b75a73001d71538ba53f
Demetria-PC\Demetria <dongmeima01@gmail.com>
2017-11-13 11:23:27 +0800

    测试

--- aes/src/com/util/aes/Main1.java
+++ aes/src/com/util/aes/Main1.java
@@ -1,8 +1,11 @@
 package com.util.aes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 
 public class Main1 {
 	public static void main(String[] args) throws UnsupportedEncodingException {
@@ -13,34 +16,65 @@ public class Main1 {
 					};
 		
 		AES aes=new AES(key);
-		String source="中国LKsnfvdjzb你找不到局还比较自动回复VB证监会不大致基本vjhbdvj";
-		char[] input=aes.aesEncoder(source);
-		
-		System.out.println("input:");
-		print(input);
-		int len1=input.length;
-		
-		long start1=System.nanoTime();
-		input=aes.aesCipher_v1(len1,input);
-		long end1=System.nanoTime();
-		System.out.println("新算法加密output:");
-		print(input);
-		
-		
-		int len=input.length;
-		input=aes.aesInvCipher(len,input);
-		System.out.println("新算法解密output:");
-		print(input);
-		System.out.println(aes.aesDecode(len1,input));
-		System.out.println("新算法用时："+(end1-start1));
+		for(int i=1;i<=15;i++){
+			String filename=i+".txt";
+			String inpath="in/"+filename;
+			String secretpath="secret_v1/"+filename;
+			String outpath="out_v1/"+filename;
+			StringBuilder sb=new StringBuilder();
+			BufferedReader br=null;
+		    try {
+				br=new BufferedReader(new FileReader(inpath));
+				String s=null;
+				while(((s=br.readLine())!=null)){
+					sb.append(System.getProperty("line.separator")+s);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally{
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			String source=sb.toString();
+			int length=source.length();
+		    char[] input=aes.aesEncoder(source);
+			
+			int len1=input.length;
+			
+			long start1=System.nanoTime();
+			input=aes.aesCipher_v1(len1,input);
+			long end1=System.nanoTime();
+			print(new String(input),secretpath);
+			
+			
+			int len=input.length;
+			input=aes.aesInvCipher(len,input);
+			String des=aes.aesDecode(len1, input);
+			print(des,outpath);
+			
+			long time=end1-start1;
+			System.out.println("原算法用时："+(time/1000000));
+		}
 		
 	}
 
-	public static void print(char[] input) {
-		int i=0;
-		for(i=0;i<input.length;i++){
-			System.out.print(input[i]);
+	public static void print(String str,String name) {
+		BufferedWriter bw=null;
+		try {
+			bw=new BufferedWriter(new FileWriter(name));
+			bw.write(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		System.out.println();
+		
 	}
 }
